34d46d6ddb5a94bab2336d48bdc7a18449798ffb
ramowtara <ashvir.ramowtar@ext.ema.europa.eu>
2023-10-20 11:45:28 +0200

    printed hello world

--- MyTestConsoleApp/Program.cs
+++ MyTestConsoleApp/Program.cs
@@ -10,6 +10,8 @@ namespace MyTestConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
